627f0820a6b9a6c66309c23fe86aaa62468f3400
Danis Zamaliev <daniszamaliev@gmail.com>
2020-10-07 18:24:40 +0300

    bump k mean

--- task_02/dot.py
+++ task_02/dot.py
@@ -1,11 +1,20 @@
 class Dot:
 
-    def __init__(self, x: int, y: int):
+    def __init__(self, x: float, y: float):
         self.x = x
         self.y = y
 
     def __str__(self) -> str:
         return 'x=' + str(self.x) + ' ' + 'y=' + str(self.y)
+
+    def __eq__(self, o: object) -> bool:
+        return self.x == o.x and self.y == o.y
+
+    def __hash__(self) -> int:
+        return hash(tuple(self.__dict__.values()))
+
+
+
 
 
 
--- task_02/kmeans.py
+++ task_02/kmeans.py
@@ -14,8 +14,6 @@ def create_dots(low: int, high: int, dot_count: int) -> []:
         dot: Dot = Dot(x=x[i], y=y[i])
         dots.append(dot)
 
-    plt.scatter(x, y)
-    plt.show()
 
     return dots
 
@@ -28,12 +26,11 @@ def get_centers(dots: [], k: int) -> []:
     dot_a: Dot = None
     dot_b: Dot = None
 
-    for i in range(0, len(dots) - 1):
+    for i in range(0, len(dots)):
         dot_from = dots[i]
-        for j in range(i + 1, len(dots) - 1):
+        for j in range(i + 1, len(dots)):
             dot_to = dots[j]
             distance = np.math.sqrt(pow((dot_from.x - dot_to.x), 2) + pow((dot_from.y - dot_to.y), 2))
-            print(distance, dot_from, dot_to)
             if distance > max_distance:
                 max_distance = distance
                 dot_a = dot_from
@@ -45,24 +42,91 @@ def get_centers(dots: [], k: int) -> []:
     dots.remove(dot_a)
     dots.remove(dot_b)
 
-    for i in range(0, k-len(center_dots)):
+    for i in range(0, k - len(center_dots)):
         max_distance = 0
-        for j in range(0, len(center_dots)-1):
-            dot_from = center_dots[j]
-            for k in range(0, len(dots)-1):
-                dot_to = dots[k]
+        new_center_dot = None
+
+        for j in range(0, len(dots)):
+            dot_from = dots[j]
+            min_distance = 0
+            for q in range(0, len(center_dots)):
+                dot_to = center_dots[q]
                 distance = np.math.sqrt(pow((dot_from.x - dot_to.x), 2) + pow((dot_from.y - dot_to.y), 2))
-                if distance > max_distance:
-                    dot_b = dot_to
+                if min_distance == 0 or distance < min_distance:
+                    min_distance = distance
 
-        center_dots.append(dot_b)
-        dots.remove(dot_b)
-
+            if max_distance < min_distance:
+                max_distance = min_distance
+                new_center_dot = dot_from
+        center_dots.append(new_center_dot)
+        dots.remove(new_center_dot)
     return center_dots
 
 
-dots = create_dots(0, 100, 100)
-center = get_centers(dots.copy(), 3)
+def create_cluster(dots: [], center_dots: []) -> {}:
+    clusters = {}
+    for dot in dots:
+        min_distance_to_center_dot = 0
+        cluster = None
 
-print(center[0], center[1], center[2])
-print(len(dots))
+        for center_dot in center_dots:
+            distance = np.math.sqrt(pow((dot.x - center_dot.x), 2) + pow((dot.y - center_dot.y), 2))
+            if min_distance_to_center_dot == 0 or distance < min_distance_to_center_dot:
+                min_distance_to_center_dot = distance
+                cluster = center_dot
+
+        cluster_dots = clusters.get(cluster) if clusters.get(cluster) is not None else []
+        cluster_dots.append(dot)
+        clusters.update({cluster: cluster_dots})
+
+    return clusters
+
+
+def recenter(clusters: {}) -> []:
+    cluster_center_dots = clusters.keys()
+    new_cluster_center = []
+    for cluster_center_dot in cluster_center_dots:
+        cluster_dots = clusters.get(cluster_center_dot)
+        center_x = 0
+        center_y = 0
+        for cluster_dot in cluster_dots:
+            center_x += cluster_dot.x
+            center_y += cluster_dot.y
+
+        center_x = center_x / len(cluster_dots)
+        center_y = center_y / len(cluster_dots)
+        new_cluster_center.append(Dot(x=center_x, y=center_y))
+
+    return new_cluster_center
+
+
+k = 3
+dots = create_dots(0, 100, 100)
+center = get_centers(dots.copy(), k)
+
+clusters = create_cluster(dots.copy(), center.copy())
+print(clusters)
+
+new_center = recenter(clusters)
+
+# print(recenter(clusters))
+#
+while center != new_center:
+    clusters = create_cluster(dots.copy(), new_center.copy())
+    center = new_center
+    new_center = recenter(clusters)
+
+for key in clusters.keys():
+    dots = clusters.get(key)
+    x = []
+    y = []
+    for dot in dots:
+        x.append(dot.x)
+        y.append(dot.y)
+
+    plt.scatter(x, y, color=np.random.rand(3,))
+    plt.scatter(key.x, key.y, color="red")
+
+plt.show()
+
+
